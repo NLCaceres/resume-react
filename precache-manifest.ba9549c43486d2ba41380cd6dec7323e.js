c68a6124edd87d57ed6776fa0122e659367dbdea
NLCaceres <NLCaceres21@Gmail.com>
2019-06-10 22:43:22 -0700

    Updates

---
+++ precache-manifest.ba9549c43486d2ba41380cd6dec7323e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb7c780ca1d697ee58e2fcc763b7383",
+    "url": "/resume-react/index.html"
+  },
+  {
+    "revision": "6618c98c2c7edf0b4daa",
+    "url": "/resume-react/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "b16efe1b9c21bb500cf1",
+    "url": "/resume-react/static/css/main.c25fe1a7.chunk.css"
+  },
+  {
+    "revision": "6618c98c2c7edf0b4daa",
+    "url": "/resume-react/static/js/2.a1dbe7fa.chunk.js"
+  },
+  {
+    "revision": "b16efe1b9c21bb500cf1",
+    "url": "/resume-react/static/js/main.4ec6b269.chunk.js"
+  },
+  {
+    "revision": "cfbea09dd05daf69739e",
+    "url": "/resume-react/static/js/runtime~main.5fa25317.js"
+  },
+  {
+    "revision": "87310a1e1b60fb7436e5e48b2948c01f",
+    "url": "/resume-react/static/media/forest.87310a1e.jpeg"
+  },
+  {
+    "revision": "cbd71f58d38b2442ddbab4f1135b5764",
+    "url": "/resume-react/static/media/mountain.cbd71f58.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume-react/precache-manifest.a95b11f37803073d802fb350cf272770.js"
+  "/resume-react/precache-manifest.ba9549c43486d2ba41380cd6dec7323e.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/resume-react/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
