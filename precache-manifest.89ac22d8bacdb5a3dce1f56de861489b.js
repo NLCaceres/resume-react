21efe2516a31511f7880cbfa8c5c69916fa3ac4b
NLCaceres <NLCaceres21@Gmail.com>
2019-12-28 00:18:43 -0800

    Updates

---
+++ precache-manifest.89ac22d8bacdb5a3dce1f56de861489b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1efb8b583f42832c30386645af36cd02",
+    "url": "/resume-react/index.html"
+  },
+  {
+    "revision": "625218a9f53dcc434ad3",
+    "url": "/resume-react/static/css/2.5c5f4c00.chunk.css"
+  },
+  {
+    "revision": "2ba79a36b977907dc5bd",
+    "url": "/resume-react/static/css/main.dd64f0a1.chunk.css"
+  },
+  {
+    "revision": "625218a9f53dcc434ad3",
+    "url": "/resume-react/static/js/2.23aabee8.chunk.js"
+  },
+  {
+    "revision": "391e1614bb076e088d450e3ace768dbf",
+    "url": "/resume-react/static/js/2.23aabee8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ba79a36b977907dc5bd",
+    "url": "/resume-react/static/js/main.78b74763.chunk.js"
+  },
+  {
+    "revision": "b05ee9a45a98b607e7b6",
+    "url": "/resume-react/static/js/runtime-main.ccb71c90.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume-react/precache-manifest.e1003078796c77236d19b28689d95bbe.js"
+  "/resume-react/precache-manifest.89ac22d8bacdb5a3dce1f56de861489b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume-react/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
